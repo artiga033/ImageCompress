fff9af2ff1fcc9f3dbdf042537119a3c91d366de
artiga033 <artiga033@hotmail.com>
2020-05-22 20:21:24 +0800

    bug fixes and improvements

--- ImageCompress/Program.cs
+++ ImageCompress/Program.cs
@@ -14,9 +14,7 @@ namespace ImageCompress
              * imagecompress [options] sourceDirectory destinationDirectory [quality]
              * source   the image file to be compressed
              * destination  specify the directory or filename of the new file
-             * options:
-             *      
-             *          
+       
             */
 
             //handle arguments
@@ -28,10 +26,10 @@ namespace ImageCompress
             var qualitySpecified = long.TryParse(args[args.Length - 1], out quality);
             quality = qualitySpecified ? quality : 80;
             int offset = qualitySpecified ? 1 : 0;
-            var outputP = args[args.Length - 1 - offset];
+            var destination = args[args.Length - 1 - offset];
             var source = args[args.Length - 1 - offset - 1];
 
-            var isValid = (source.IsFile() || source.IsDirectory()) && (outputP.IsDirectory() || outputP.IsFile());
+            var isValid = (source.IsFile() || source.IsDirectory()) && (destination.IsDirectory() || destination.IsFile());
             if (!isValid)
             {
                 PrintHelpText(); return;
@@ -39,34 +37,41 @@ namespace ImageCompress
             //source is a single file
             if (File.Exists(source))
             {
-                if (outputP.IsFile()) CompressHandler.Compress(source, outputP, quality);
+                if (destination.IsFile()) CompressHandler.Compress(source, destination, quality);
                 else
                 {
-                    if (Directory.Exists(outputP))
+                    if (Directory.Exists(destination))
                     {
+
+                        var outputPath = destination.IsDirectory() ?
+                            Path.Combine(destination, Path.GetFileNameWithoutExtension(source)) + ".jpg"
+                            : destination;//destination could be either a directory or a file path.
                         try
                         {
-                            CompressHandler.Compress(source, Path.Combine(outputP, Path.GetFileNameWithoutExtension(source)) + ".jpg", quality);
+                            CompressHandler.Compress(source,outputPath , quality);
                         }
                         catch (Exception e)
                         {
                             Console.WriteLine($"Error with{source},{e.Message},{e.StackTrace}");
                         }
                     }
-                    else Console.WriteLine("Directory not exists" + outputP);
+                    else Console.WriteLine("Directory not exists" + destination);
                 }
             }
             //source is a Directory
             else if (Directory.Exists(source))
             {
-                if (!Directory.Exists(outputP)) Directory.CreateDirectory(outputP);
+                if (!Directory.Exists(destination)) Directory.CreateDirectory(destination);
                 var filesList = new DirectoryAnalyst(source).GetImageFilesList();
                 Parallel.ForEach(filesList, (filename) =>
                   {
+                      //keep directory struct
                       var relativePath = Path.GetRelativePath(source, filename);
-                      var targetPath = Path.GetFullPath(relativePath, outputP);
+                      var targetPath = Path.GetFullPath(relativePath, destination);
+
                       var dir = Path.GetDirectoryName(targetPath);
                       if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
                       try
                       {
                           CompressHandler.Compress(filename, targetPath, quality);
@@ -86,11 +91,10 @@ namespace ImageCompress
         static void PrintHelpText()
         {
             Console.WriteLine(@"Help:
-* imagecompress source destination [quality]
-* imagecompress[options] sourceDirectory destinationDirectory [quality]
-* source   the image file to be compressed
-* destination  specify the directory or filename of the new file
-* quality   quality argument for the jpeg encoder. Default:80
+*   imagecompress source destination [quality]
+*   source        the image file or a directory which contains files to be compressed
+*   destination   specify the directory or filename of the new file
+*   quality       quality argument for the jpeg encoder. Default:80
 *
 *");
         }
